fd0762e3818e12d86cf945848bdf282a8fceebf7
Chris French <chris@ammonitenetworks.com>
2013-11-22 11:54:28 -0800

    adding ajax submitting to display trac form

--- index.php
+++ index.php
@@ -181,6 +181,43 @@ $f3->route('GET /m',
 
 	}
 );
+$f3->route('POST /ajax/upload',
+  function($f3) {
+
+    nodePusher($f3->get('SESSION.id').'home', $f3->get('SESSION.id').'index2');
+       //get the file that we uploaded
+      $file = $_FILES['uploader'];
+
+
+$overwrite = false; // set to true, to overwrite an existing file; Default: false
+$slug = true; // rename file to filesystem-friendly version
+$web = Web::instance();
+$image = $web->receive(function($file){
+  
+        /* looks like:
+          array(5) {
+              ["name"] =>     string(19) "csshat_quittung.png"
+              ["type"] =>     string(9) "image/png"
+              ["tmp_name"] => string(14) "/tmp/php2YS85Q"
+              ["error"] =>    int(0)
+              ["size"] =>     int(172245)
+            }
+        */
+        // maybe you want to check the file size
+       // if($file['size'] > (2 * 1024 * 1024)) // if bigger than 2 MB
+        //    return false; // this file is not valid, return false will skip moving it
+
+        // everything went fine, hurray!
+        return true; // allows the file to be moved from php tmp dir to your defined upload dir
+    },
+    $overwrite,
+    $slug
+);
+
+
+
+
+
 
 
 $f3->route('POST /m/index2',
@@ -215,6 +252,45 @@ $image = $web->receive(function($file){
     $overwrite,
     $slug
 );
+$image = array_keys($image); //this is dumb
+
+
+//save to database
+$db=new DB\Jig('db/',DB\Jig::FORMAT_JSON);
+$dash=new DB\Jig\Mapper($db,'proofofplacement');
+$dash->load(array('@dash=?','1'));
+
+$placements = $dash->placements;
+$date = new DateTime();
+$id = count($placements) + 1;
+$array = array(
+    "id" => $id,
+    "href" => $image[0],
+        "address" =>"122 S Pine Street Salt Lake City, UT 84037",
+        "time" => $date->format('Y-m-d H:i:s'),
+        "img" => $image[0]
+  );
+
+$placements[] = $array;
+$dash->placements = $placements;
+$dash->save();
+
+
+//event to pusher
+$app_id = '59967';
+$app_key = 'f3b8b0aeaf31c105168e';
+$app_secret = '87a99b695fda2400d4fd';
+
+$pusher = new Pusher( $app_key, $app_secret, $app_id );
+  $event =  $pusher->trigger('images', 'addimage' , $array );
+
+
+    $view=new View;
+        echo $view->render('m/index2.html');
+
+  }
+);
+
 
 
 $image = array_keys($image); //this is dumb
